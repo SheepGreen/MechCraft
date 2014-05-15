be7b9dffd678e8ed260e656fafbedec0b3a7332e
SheepGreen <sturk10isepic@gmail.com>
2014-05-14 22:09:29 -0700

    Another Commit

--- src/main/java/me/sturk/RealCraft/Cable.java
+++ src/main/java/me/sturk/RealCraft/Cable.java
@@ -4,14 +4,21 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class Cable extends Block {
+
+    private static int Side;
+
     public Cable(Material material) {
         super(material);
         setCreativeTab(Main.realTab);
         setBlockBounds(0.4F, 0.4F, 0.4F, 0.6F, 0.6F, 0.6F);
+
+        if(Side==0) {
+
+        }
     }
 
-    public void CableInfo(int side, float x, float y, float z) {
-
+    public static void CableInfo(int side, int x, int y, int z, Block block) {
+        Side = side;
     }
 
     public boolean renderAsNormalBlock() {
--- src/main/java/me/sturk/RealCraft/ItemCable.java
+++ src/main/java/me/sturk/RealCraft/ItemCable.java
@@ -6,14 +6,26 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemCable extends Item {
-    public ItemCable() {
-        super();
-    }
 
     public boolean onItemUse(ItemStack itemstack, EntityPlayer player, World world, int x, int y, int z, int side, float x2, float y2, float z2) {
         if(!world.isRemote) {
+            if(side==0) {
+                world.setBlock(x,y-1,z,Main.cable);
+                Cable.CableInfo(0, x, y-1, z, world.getBlock(x,y,z));
+            }else if(side==1) {
+                world.setBlock(x,y+1,z,Main.cable);
+            }else if(side==2) {
+                world.setBlock(x,y,z-1,Main.cable);
+            }else if(side==3) {
+                world.setBlock(x,y,z+1,Main.cable);
+            }else if(side==4) {
+                world.setBlock(x-1,y,z,Main.cable);
+            }else if(side==5) {
+                world.setBlock(x+1,y,z,Main.cable);
+            }
             System.out.println(side);
+            return true;
         }
-        return true;
+        return false;
     }
 }
