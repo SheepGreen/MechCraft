d5ddfddeda2018523223cc40661e4c16c1f8c236
SheepGreen <sturk10isepic@gmail.com>
2014-05-15 22:14:44 -0700

    Going to Bed

--- src/main/java/me/sturk/RealCraft/Main.java
+++ src/main/java/me/sturk/RealCraft/Main.java
@@ -4,12 +4,18 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid=Main.Modid, name=Main.Name, version=Main.Version)
 public class Main {
@@ -20,23 +26,31 @@ public class Main {
     public static final String Version = "1.0";
     public static CreativeTabs realTab = new RealTab("RealCraft");
     public static Item screwDriver;
-    public static Item itemCable;
     public static Block cable;
+    public static Block generator;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    public void preinit(FMLPreInitializationEvent event) {
         screwDriver = new ScrewDriver().setUnlocalizedName("ScrewDriver");
-        itemCable = new ItemCable().setUnlocalizedName("ItemCable");
         GameRegistry.registerItem(screwDriver, "ScrewDriver");
-        GameRegistry.registerItem(itemCable, "ItemCable");
 
+        generator = new Generator(Material.iron).setBlockName("Generator");
         cable = new Cable(Material.iron).setBlockName("Cable");
         GameRegistry.registerBlock(cable, "Cable");
+
+        GameRegistry.addRecipe(new ItemStack(generator, 1), new Object[]{
+                "iii", "ifi", "iii", 'i', Items.iron_ingot, 'f', Blocks.furnace
+        });
     }
 
     @EventHandler
-    public void Init(FMLInitializationEvent event) {
+    public void init(FMLInitializationEvent event) {
         GameRegistry.registerTileEntity(TileEntityCable.class, "cable");
         proxy.registerRenderers();
+    }
+
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new GuiGenerator(Minecraft.getMinecraft()));
     }
 }
